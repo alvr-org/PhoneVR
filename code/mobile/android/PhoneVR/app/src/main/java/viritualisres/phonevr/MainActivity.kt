f4808f0e6034e70226a684148187e2aff9b4011f
Harsha Raghu <funnyboy313@gmail.com>
2020-12-19 08:58:06 +0530

    Update MainActivity.kt for getting Logcat Logs

--- code/mobile/android/PhoneVR/app/src/main/java/viritualisres/phonevr/MainActivity.kt
+++ code/mobile/android/PhoneVR/app/src/main/java/viritualisres/phonevr/MainActivity.kt
@@ -14,21 +14,42 @@ import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import viritualisres.phonevr.Wrap.deinitializeNativeCrashHandler
-import viritualisres.phonevr.Wrap.initializeNativeCrashHandler
-
-import viritualisres.phonevr.BuildConfig;
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private val REQUEST_CODE: Int = 52142;
 
+    fun launchLogcat(filename: String): Process? {
+        var process: Process? = null
+        val cmd = "logcat -f $filename\n"
+        process = try {
+            Runtime.getRuntime().exec(cmd)
+        } catch (e: IOException) {
+            null
+        }
+        return process
+    }
+
+    private lateinit var logcatProcess : Process
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
-        initializeNativeCrashHandler();
+        /*try {
+            val filename = File(getExternalFilesDir(null).toString() + "/PVR/logcatfile.log")
+            filename.createNewFile()
+            val cmd = "logcat -v -f " + filename.getAbsolutePath()
+            Runtime.getRuntime().exec(cmd)
+        } catch (e: IOException) {
+            // TODO Auto-generated catch block
+            e.printStackTrace()
+        }*/
+        logcatProcess = launchLogcat(getExternalFilesDir(null).toString() + "/PVR/logcatfile.log")!!
+        //initializeNativeCrashHandler();
 
+        //Runtime.getRuntime().exec("adb bugreport " + getExternalFilesDir(null).toString() + "/PVR/bugreport/");
         setExtDirinJNI();
 
         setContentView(R.layout.activity_main)
@@ -63,7 +84,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setExtDirinJNI() {
         val dir = getExternalFilesDir(null).toString()
-        Log.d("PhoneVR", "ExtDir: " + dir + ", ExtRead Only? :"+ isExternalStorageReadOnly() + ", ExtAvalb ?:"+ isExternalStorageAvailable())
+        Log.d("PhoneVR", "ExtDir: " + dir + ", ExtRead Only? :" + isExternalStorageReadOnly() + ", ExtAvalb ?:" + isExternalStorageAvailable())
 
         //try{
         Wrap.setExtDirectory(dir, dir.length)
@@ -90,13 +111,12 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             REQUEST_CODE -> {
                 if ((grantResults.isEmpty() ||
-                        grantResults[0] != PackageManager.PERMISSION_GRANTED) ||
+                                grantResults[0] != PackageManager.PERMISSION_GRANTED) ||
                         grantResults[1] != PackageManager.PERMISSION_GRANTED) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE)
                     }
-                }
-                else
+                } else
                     setExtDirinJNI()
                 return
             }
@@ -115,14 +135,14 @@ class MainActivity : AppCompatActivity() {
         //Log.d("PhoneVR", "OnResume called... Starting Announcer @ port :" + prefs.getInt(connPortKey, connPortDef).toString());
         prefs.getString(pcIpKey, pcIpDef)?.let { Wrap.startAnnouncer(it, prefs.getInt(connPortKey, connPortDef)) }
 
-        initializeNativeCrashHandler();
+        //initializeNativeCrashHandler();
     }
 
     override fun onPause() {
         super.onPause()
         Wrap.stopAnnouncer()
 
-        deinitializeNativeCrashHandler();
+        //deinitializeNativeCrashHandler();
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
