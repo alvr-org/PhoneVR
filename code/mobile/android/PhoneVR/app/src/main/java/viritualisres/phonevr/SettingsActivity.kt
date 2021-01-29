ce1b60f7c29f1e743548bacf27d16b9bb92431f6
Harsha Raghu <funnyboy313@gmail.com>
2021-01-29 22:53:24 +0530

    [Android] Add ShareLogs and OpenLogs buttons (#65)
    Added ShareLogs and OpenLogs buttons in Settings Activity

--- code/mobile/android/PhoneVR/app/src/main/java/viritualisres/phonevr/SettingsActivity.kt
+++ code/mobile/android/PhoneVR/app/src/main/java/viritualisres/phonevr/SettingsActivity.kt
@@ -1,21 +1,25 @@
 package viritualisres.phonevr
 
-
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View.OnTouchListener
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_settings.*
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.IOException
-import java.io.RandomAccessFile
+import java.io.*
 import java.util.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -40,6 +44,151 @@ class SettingsActivity : AppCompatActivity() {
             false
         }
         etLogView.setOnTouchListener(touchListener)
+
+        val btShareLogs: Button  = findViewById(R.id.btShareLogs)
+        btShareLogs.setOnClickListener {
+            val intentShareFile = Intent(Intent.ACTION_SEND)
+
+            val pvrFilesFolder = getExternalFilesDir(null).toString() + "/PVR"
+            val pvrZipFilesFolder = getExternalFilesDir(null).toString() + "/PVR.zip"
+
+            val fileZip = File(pvrZipFilesFolder)
+            val fileWithinMyDir = File(pvrFilesFolder)
+
+            if (fileWithinMyDir.exists()) {
+                if (fileWithinMyDir.listFiles()?.size!! > 1) {
+                    if(fileZip.exists())
+                        fileZip.delete()
+
+                    zipFileAtPath( pvrFilesFolder, pvrZipFilesFolder)
+                    intentShareFile.type = "application/zip"
+                    intentShareFile.putExtra(Intent.EXTRA_STREAM,
+                            FileProvider.getUriForFile(Objects.requireNonNull(applicationContext),
+                                    BuildConfig.APPLICATION_ID + ".provider",
+                                    fileZip))
+                }
+                else {
+                    intentShareFile.type = "application/txt"
+                    intentShareFile.putExtra(Intent.EXTRA_STREAM,
+                            FileProvider.getUriForFile(Objects.requireNonNull(applicationContext),
+                                    BuildConfig.APPLICATION_ID + ".provider",
+                                    File(getExternalFilesDir(null).toString() + "/PVR/pvrlog.txt")))
+                }
+                intentShareFile.putExtra(Intent.EXTRA_SUBJECT,
+                        "Sharing pvrlog File(s)...")
+                intentShareFile.putExtra(Intent.EXTRA_TEXT, "Sharing pvrlog File(s)...")
+                startActivity(Intent.createChooser(intentShareFile, "Share File"))
+            }
+        }
+
+        val btOpenLogs: Button  = findViewById(R.id.btOpenLogs)
+        btOpenLogs.setOnClickListener {
+            val dir = File(getExternalFilesDir(null).toString() + "/PVR/")
+            if (dir.listFiles()?.size!! > 1) {
+                val items = arrayOfNulls<CharSequence>(2)
+                items[0] = "pvrlog.txt"; items[1] = "pvrDebuglog.txt"
+
+                val builder = AlertDialog.Builder(this@SettingsActivity)
+                builder.setTitle(R.string.pick_logfile)
+                        .setItems(R.array.logsFiles, DialogInterface.OnClickListener { _, which ->
+                            Log.d("PVR_JAVA", "Which $which")
+                            OpenFileWithIntent(File(getExternalFilesDir(null).toString() + "/PVR/" + items[which]))
+                        })
+                builder.create()
+                builder.show()
+            } else {
+                OpenFileWithIntent(File(getExternalFilesDir(null).toString() + "/PVR/pvrlog.txt"))
+            }
+        }
+    }
+    /*
+    * Zips a file at a location and places the resulting zip file at the toLocation
+    * Example: zipFileAtPath("downloads/myfolder", "downloads/myFolder.zip");
+    */
+    private fun zipFileAtPath(sourcePath: String, toLocation: String?): Boolean {
+        val BUFFER = 2048
+        val sourceFile = File(sourcePath)
+        try {
+            var origin: BufferedInputStream? = null
+            val dest = FileOutputStream(toLocation)
+            val out = ZipOutputStream(BufferedOutputStream(
+                    dest))
+            if (sourceFile.isDirectory) {
+                zipSubFolder(out, sourceFile, sourceFile.parent.length)
+            } else {
+                val data = ByteArray(BUFFER)
+                val fi = FileInputStream(sourcePath)
+                origin = BufferedInputStream(fi, BUFFER)
+                val entry = ZipEntry(getLastPathComponent(sourcePath))
+                entry.setTime(sourceFile.lastModified()) // to keep modification time after unzipping
+                out.putNextEntry(entry)
+                var count: Int = 0
+                while (origin.read(data, 0, BUFFER).also({ count = it }) != -1) {
+                    out.write(data, 0, count)
+                }
+            }
+            out.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            return false
+        }
+        return true
+    }
+
+    /*
+    * Zips a subfolder
+    */
+    @Throws(IOException::class)
+    private fun zipSubFolder(out: ZipOutputStream, folder: File,
+                             basePathLength: Int) {
+        val BUFFER = 2048
+        val fileList = folder.listFiles()
+        var origin: BufferedInputStream? = null
+        for (file in fileList) {
+            if (file.isDirectory) {
+                zipSubFolder(out, file, basePathLength)
+            } else {
+                val data = ByteArray(BUFFER)
+                val unmodifiedFilePath = file.path
+                val relativePath = unmodifiedFilePath
+                        .substring(basePathLength)
+                val fi = FileInputStream(unmodifiedFilePath)
+                origin = BufferedInputStream(fi, BUFFER)
+                val entry = ZipEntry(relativePath)
+                entry.setTime(file.lastModified()) // to keep modification time after unzipping
+                out.putNextEntry(entry)
+                var count: Int = 0
+                while (origin.read(data, 0, BUFFER).also({ count = it }) != -1) {
+                    out.write(data, 0, count)
+                }
+                origin.close()
+            }
+        }
+    }
+
+    /*
+    * gets the last path component
+    *
+    * Example: getLastPathComponent("downloads/example/fileToZip");
+    * Result: "fileToZip"
+    */
+    private fun getLastPathComponent(filePath: String): String? {
+        val segments = filePath.split("/".toRegex()).toTypedArray()
+        return if (segments.size == 0) "" else segments[segments.size - 1]
+    }
+
+    private fun OpenFileWithIntent(file: File)
+    {
+        // Get URI and MIME type of file
+        val uri = FileProvider.getUriForFile(Objects.requireNonNull(applicationContext), BuildConfig.APPLICATION_ID + ".provider", file)
+        val mime = contentResolver.getType(uri)
+
+        // Open file with user selected app
+        val intent = Intent()
+        intent.action = Intent.ACTION_VIEW
+        intent.setDataAndType(uri, mime)
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        startActivity(intent)
     }
 
     private fun setTextToLogger(et: TextView){
