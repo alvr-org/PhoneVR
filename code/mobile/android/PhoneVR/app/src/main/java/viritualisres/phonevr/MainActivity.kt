a04abaaada6b9ddc66065c9f061abf64c945a0b6
Harsha Raghu <narnindi.raghu@gmail.com>
2023-04-12 18:57:14 +0530

    [Android] Make PhoneVR emulator friendly
    - Now automatically assigns 10.0.2.2 (default emu addr) to pcIP if PVR android is run on APK
    - still need to run "adb forward tcp:33333 tcp:33333" after android app started on android emulator

--- code/mobile/android/PhoneVR/app/src/main/java/viritualisres/phonevr/MainActivity.kt
+++ code/mobile/android/PhoneVR/app/src/main/java/viritualisres/phonevr/MainActivity.kt
@@ -14,6 +14,7 @@ import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_settings.*
 
 import viritualisres.phonevr.BuildConfig;
 
@@ -24,6 +25,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
+        // TODO: Add log to file, by making some util
+        Log.d("PhoneVR", "Running PVR on FINGERPRINT:${Build.FINGERPRINT}\n" +
+            "MODEL:${Build.MODEL}\n" +
+            "MANUFACTURER:${Build.MANUFACTURER}\n" +
+            "BRAND:${Build.BRAND}\n" +
+            "DEVICE:${Build.DEVICE}\n" +
+            "BOARD:${Build.BOARD}\n" +
+            "HOST:${Build.HOST}\n" +
+            "PRODUCT:${Build.PRODUCT}\n");
 
         setExtDirinJNI();
 
