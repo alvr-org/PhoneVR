134e176961b646478a3a4a6f5731db00a37695e9
Harsha Raghu <narnindi.raghu@gmail.com>
2023-07-27 18:23:38 +0530

    Code cleanup (#161)

--- code/mobile/android/opencv.sh
+++
@@ -1,8 +0,0 @@
-export ANDROID_NDK=/mnt/c/android-ndk-r13b
-OCVPATH=/mnt/c/opencv/sources
-
-cd ${OCVPATH}/platforms/android
-mkdir build
-cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=${OCVPATH}/platforms/android/android.toolchain.cmake ..
-make -j8
